72899a17a87da926ccb08755d664659774f62c27
Iamjava <jan.vaorin@tu-dortmund.de>
2024-06-18 11:59:10 +0200

    replication now added

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # Little Share
 
-I needed a little webserver just to share files in my VPN. Maybe ssh would be enough but I like web-frontends. 
+I needed a little webserver just to share files in my VPN. Maybe ssh would be enough but I like web-frontends.
 Also I wanted to learn cross-compiling for ARM.
 
 ```shell
 alias rust-musl-builder='docker run --rm -it -v "$(pwd)":/home/rust/src messense/rust-musl-cross:armv7-musleabihf'                                                  I
 rust-musl-builder cargo build --release
 ```
+
 ```shell
 scp target/armv7-unknown-linux-musleabihf/release/little-share user@pi:/home/user/location
 ```
+
 ![image](https://github.com/corgijan/little-share/assets/70795482/c1396f5c-cec8-49ac-b00a-0960c238f5a2)
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-use std::fmt::format;
-use axum::{extract::{Multipart, Path, Query}, response::{Html, IntoResponse, Redirect, Response}, routing::{get, post}, Router};
-use futures_util::stream::StreamExt;
 use std::fs;
-use std::str::FromStr;
+
+use axum::{extract::{Multipart, Path}, response::{Html, IntoResponse, Redirect, Response}, Router, routing::{get, post}};
 use axum::body::{Body, Bytes};
 use axum::http::header;
-use mime_guess::Mime;
+use futures_util::stream::StreamExt;
 
 static PW_CHECK: bool = true;
 static PASSWORD: &str = "password";
@@ -29,9 +27,9 @@ input[type='file'] { margin-bottom: 1em; }\
 </style>";
 
 async fn upload(mut multipart: Multipart) -> impl IntoResponse {
-    let mut name_field= None;
+    let mut name_field = None;
     let mut original_file_name = None;
-    let mut original_file_ending= None;
+    let mut original_file_ending = None;
     let mut data = None;
 
     while let Some(mut field) = multipart.next_field().await.unwrap() {
@@ -44,7 +42,6 @@ async fn upload(mut multipart: Multipart) -> impl IntoResponse {
                     name_field = Some(bind);
                 }
             }
-
         } else if field.name() == Some("file") {
             original_file_name = Some(dbg!(field.file_name().unwrap().to_string()));
             if original_file_name.clone().unwrap().contains(".") {
@@ -60,22 +57,20 @@ async fn upload(mut multipart: Multipart) -> impl IntoResponse {
         let mut file_name;
         if original_file_ending.is_some() {
             file_name = format!("{}.{}", &name_field.unwrap(), &original_file_ending.unwrap());
-        }else {
+        } else {
             file_name = name_field.unwrap();
         }
         write_file(file_name, data.unwrap()).await.unwrap();
         Redirect::to("/").into_response()
-
-
-    } else if name_field.is_none() && data.is_some() && original_file_name.is_some() && original_file_ending.is_some() && original_file_ending.is_some(){
+    } else if name_field.is_none() && data.is_some() && original_file_name.is_some() && original_file_ending.is_some() && original_file_ending.is_some() {
         write_file(original_file_name.unwrap(), data.unwrap()).await.unwrap();
         Redirect::to("/").into_response()
-    } else if name_field.is_none() && data.is_some() && original_file_name.is_some() && original_file_ending.is_none() && original_file_ending.is_some(){
+    } else if name_field.is_none() && data.is_some() && original_file_name.is_some() && original_file_ending.is_none() && original_file_ending.is_some() {
         write_file(original_file_name.unwrap(), data.unwrap()).await.unwrap();
         Redirect::to("/").into_response()
     } else {
         Response::builder().status(400).body("Bad Request".into()).unwrap()
-    }
+    };
 }
 
 async fn upload_form() -> Html<String> {
@@ -105,19 +100,22 @@ async fn upload_form() -> Html<String> {
     ))
 }
 
-async fn show(Path(file_name): Path<String>,download: bool) -> Response{
+async fn show(Path(file_name): Path<String>, download: bool) -> Response {
     let main_loc = get_main_loc();
-    let file_path = format!("{}/{}",main_loc, file_name);
+    let file_path = format!("{}/{}", main_loc, file_name);
     if let Ok(data) = fs::read(file_path) {
         let body = Body::from(data);
         let guess = mime_guess::from_path(file_name.clone()).first_or_octet_stream();
-        let mut headers = vec![
+        let mut headers = [
             (header::CONTENT_TYPE, dbg!(guess.to_string())),
         ];
+
         if download {
-            headers.append(&mut vec![(header::CONTENT_DISPOSITION, format!("attachment; filename=\"{:?}\"", file_name.clone()))]);
+            headers = [
+                (header::CONTENT_DISPOSITION, format!("attachment; filename=\"{:?}\"", file_name.clone()))
+            ]
         }
-        (headers.into(), body).into_response()
+        (headers, body).into_response()
     } else {
         Response::builder()
             .status(404)
@@ -128,32 +126,31 @@ async fn show(Path(file_name): Path<String>,download: bool) -> Response{
 
 async fn write_file(file_name: String, data: Bytes) -> Result<(), std::io::Error> {
     let locations = std::env::var("REPLICATION_LOCATIONS").expect("REPLICATION_LOCATION not set, please set in ENV or .env file");
-    for loc in locations.split(":"){
-        fs::write(format!("{}/{}",loc, file_name), data.clone()).expect(format!("Failed to write file to {}", loc).as_str());
+    for loc in locations.split(":") {
+        fs::write(format!("{}/{}", loc, file_name), data.clone()).expect(format!("Failed to write file to {}", loc).as_str());
     }
     Ok(())
 }
 
-fn get_main_loc() -> String{
+fn get_main_loc() -> String {
     let bind = std::env::var("REPLICATION_LOCATIONS").expect("REPLICATION_LOCATION not set, please set in ENV or .env file");
     let main_loc = bind.split(":").collect::<Vec<&str>>()[0];
     main_loc.to_string()
 }
 
-fn generate_all_folders(){
+fn generate_all_folders() {
     let locations = std::env::var("REPLICATION_LOCATIONS").expect("REPLICATION_LOCATION not set, please set in ENV or .env file");
-    for loc in locations.split(":"){
+    for loc in locations.split(":") {
         fs::create_dir_all(loc).expect(format!("Failed to create directory {}", loc).as_str());
     }
-
 }
 
-fn sync_files(){
-    for loc2 in std::env::var("REPLICATION_LOCATIONS").unwrap().split(":"){
-        for loc1 in std::env::var("REPLICATION_LOCATIONS").unwrap().split(":"){
+fn sync_files() {
+    for loc2 in std::env::var("REPLICATION_LOCATIONS").unwrap().split(":") {
+        for loc1 in std::env::var("REPLICATION_LOCATIONS").unwrap().split(":") {
             if loc1 != loc2 {
                 println!("Syncing {} to {}", loc1, loc2);
-                for file in fs::read_dir(loc1).expect("Failed to read main directory"){
+                for file in fs::read_dir(loc1).expect("Failed to read main directory") {
                     let file = file.unwrap();
                     // if file is not in loc2 write it to loc2
                     if !fs::metadata(format!("{}/{}", loc2, file.file_name().to_str().unwrap())).is_ok() {
@@ -183,6 +180,6 @@ async fn main() {
     sync_files();
     println!("Syncing done");
 
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}",port)).await.unwrap();
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port)).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
